0bd313ad41643975a899ef6303afca7d1d62ed57
sonumoraniya <techtic.sonu@gmail.com>
2026-08-27 10:34:24 +0530

    deleted files

--- src/components/ui/ErrorState.tsx
+++
@@ -1,5 +0,0 @@
-import { Alert } from "@/components/ui/Alert";
-
-export function ErrorState({ message, onRetry }: { message: string; onRetry?: () => void }) {
-  return <Alert message={message} onRetry={onRetry} />;
-}
